aa91c576d28395a6a6b1a2e23ef5c2c004dd36e2
Andrés Almada <aalmada@ids.net.ar>
2020-06-23 23:13:17 -0300

    commit

---
+++ build/precache-manifest.d3fab87053f711c839f5e1119cfea79c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51bead61764bca3eacd4984cd17815d7",
+    "url": "/academic-redditer/index.html"
+  },
+  {
+    "revision": "e20566b663d1bb94c2f0",
+    "url": "/academic-redditer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0480e28a94bf0ec8fc30",
+    "url": "/academic-redditer/static/js/2.2fff5628.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/academic-redditer/static/js/2.2fff5628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e20566b663d1bb94c2f0",
+    "url": "/academic-redditer/static/js/main.332c9259.chunk.js"
+  },
+  {
+    "revision": "5f9c9de58392a5f151c9",
+    "url": "/academic-redditer/static/js/runtime-main.f00aba93.js"
+  }
+]);
