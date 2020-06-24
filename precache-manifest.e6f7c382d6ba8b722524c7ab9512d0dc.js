a00ee734cd579e9cdb468d42671d73310a06dfb2
Andrés Almada <aalmada@ids.net.ar>
2020-06-23 23:07:44 -0300

    Updates

---
+++ precache-manifest.e6f7c382d6ba8b722524c7ab9512d0dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df424bea5a584e6a84a239187480f84b",
+    "url": "/academicredditer/index.html"
+  },
+  {
+    "revision": "8fc79740627d3d0d06d8",
+    "url": "/academicredditer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bd195fb90b74149900fe",
+    "url": "/academicredditer/static/js/2.3b0a76ea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/academicredditer/static/js/2.3b0a76ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fc79740627d3d0d06d8",
+    "url": "/academicredditer/static/js/main.6b1ccb3f.chunk.js"
+  },
+  {
+    "revision": "c12a88ac79a670313cdd",
+    "url": "/academicredditer/static/js/runtime-main.0520ee9f.js"
+  }
+]);
